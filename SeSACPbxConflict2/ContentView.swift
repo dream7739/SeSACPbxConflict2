b5017c7fc5c133afdd18b487925c7232d53265bb
홍정민 <dream7739@naver.com>
2024-10-08 14:16:57 +0900

    init

---
+++ SeSACPbxConflict2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSACPbxConflict2
+//
+//  Created by 홍정민 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeSACPbxConflict2/SeSACPbxConflict2App.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACPbxConflict2App.swift
+//  SeSACPbxConflict2
+//
+//  Created by 홍정민 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SeSACPbxConflict2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
